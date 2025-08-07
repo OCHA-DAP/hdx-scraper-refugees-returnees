ae928888ba2e33997b526bc2a3fbc9d98eb030df
Briar <briar.j.mills@gmail.com>
2025-08-07 13:02:43 -0400

    Refactor to latest setup

---
+++ run.py
@@ -0,0 +1,4 @@
+import runpy
+
+# Execute a module by its full module name
+runpy.run_module("hdx.scraper.refugees_returnees", run_name="__main__")
--- src/hdx/scraper/refugees_returnees/__main__.py
+++ src/hdx/scraper/refugees_returnees/__main__.py
@@ -13,54 +13,58 @@ from hdx.api.utilities.hdx_error_handler import HDXErrorHandler
 from hdx.data.user import User
 from hdx.facades.infer_arguments import facade
 from hdx.utilities.downloader import Download
-from hdx.utilities.path import script_dir_plus_file, temp_dir_batch
+from hdx.utilities.path import (
+    script_dir_plus_file,
+    temp_dir_batch,
+)
 from hdx.utilities.retriever import Retrieve
 
-from hdx.scraper.refugees_returnees.refugees_returnees import RefugeesReturnees
+from hdx.scraper.refugees_returnees._version import __version__
+from hdx.scraper.refugees_returnees.pipeline import Pipeline
 
 logger = logging.getLogger(__name__)
 
-_USER_AGENT_LOOKUP = "hdx-scraper-refugees-returnees"
+_LOOKUP = "hdx-scraper-refugees-returnees"
 _SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
 _UPDATED_BY_SCRIPT = "HDX Scraper: refugees-returnees"
 
 
 def main(
-    save: bool = True,
+    save: bool = False,
     use_saved: bool = False,
     err_to_hdx: bool = False,
 ) -> None:
     """Generate datasets and create them in HDX
 
     Args:
-        save (bool): Save downloaded data. Defaults to True.
+        save (bool): Save downloaded data. Defaults to False.
         use_saved (bool): Use saved data. Defaults to False.
         err_to_hdx (bool): Whether to write any errors to HDX metadata. Defaults to False.
 
     Returns:
         None
     """
+    logger.info(f"##### {_LOOKUP} version {__version__} ####")
     configuration = Configuration.read()
-    if not User.check_current_user_organization_access("hdx", "create_dataset"):
-        raise PermissionError("API Token does not give access to HDX organisation!")
+    User.check_current_user_write_access("hdx")
 
     with HDXErrorHandler(write_to_hdx=err_to_hdx) as error_handler:
-        with temp_dir_batch(folder=_USER_AGENT_LOOKUP) as info:
-            temp_dir = info["folder"]
+        with temp_dir_batch(folder=_LOOKUP) as info:
+            tempdir = info["folder"]
             with Download() as downloader:
                 retriever = Retrieve(
                     downloader=downloader,
-                    fallback_dir=temp_dir,
+                    fallback_dir=tempdir,
                     saved_dir=_SAVED_DATA_DIR,
-                    temp_dir=temp_dir,
+                    temp_dir=tempdir,
                     save=save,
                     use_saved=use_saved,
                 )
-                ref_ret = RefugeesReturnees(configuration, retriever, error_handler)
-                dataset_types = ref_ret.get_data()
+                pipeline = Pipeline(configuration, retriever, error_handler)
+                dataset_types = pipeline.get_data()
 
                 for dataset_type in dataset_types:
-                    dataset = ref_ret.generate_dataset(dataset_type)
+                    dataset = pipeline.generate_dataset(dataset_type)
                     dataset.update_from_yaml(
                         path=script_dir_plus_file(
                             join(
@@ -83,7 +87,7 @@ if __name__ == "__main__":
     facade(
         main,
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
-        user_agent_lookup=_USER_AGENT_LOOKUP,
+        user_agent_lookup=_LOOKUP,
         project_config_yaml=script_dir_plus_file(
             join("config", "project_configuration.yaml"), main
         ),
