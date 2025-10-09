1412e603b052e6651929fedfb7a556aeaaf391bd
Briar <briar.j.mills@gmail.com>
2025-10-09 14:28:39 -0400

    Upgrade requirements (#5)

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -57,13 +57,8 @@ class TestPipeline:
                     assert resources == [
                         {
                             "name": "Global Affected People: Returnees",
-                            "description": "Returnees data from HDX HAPI, please see "
-                            "[the documentation](https://hdx-hapi.readthedocs.io/en/"
-                            "latest/data_usage_guides/affected_people/#returnees) for "
-                            "more information",
+                            "description": "Returnees data from HDX HAPI, please see [the documentation](https://hdx-hapi.readthedocs.io/en/latest/data_usage_guides/affected_people/#returnees) for more information",
                             "format": "csv",
-                            "resource_type": "file.upload",
-                            "url_type": "upload",
                         }
                     ]
 
@@ -75,8 +70,7 @@ class TestPipeline:
                     dataset = pipeline.generate_dataset("refugees")
                     assert dataset == {
                         "name": "hdx-hapi-refugees",
-                        "title": "HDX HAPI - Affected People: Refugees & Persons of "
-                        "Concern",
+                        "title": "HDX HAPI - Affected People: Refugees & Persons of Concern",
                         "dataset_date": "[2020-01-01T00:00:00 TO 2023-12-31T23:59:59]",
                         "tags": [
                             {
@@ -90,15 +84,8 @@ class TestPipeline:
                     resources = dataset.get_resources()
                     assert resources == [
                         {
-                            "name": "Global Affected People: Refugees & Persons of "
-                            "Concern (2020-2024)",
-                            "description": "Refugees and Persons of Concern data (2020-"
-                            "2024) from HDX HAPI, please see [the documentation]"
-                            "(https://hdx-hapi.readthedocs.io/en/latest/data_usage_guides"
-                            "/affected_people/#refugees-persons-of-concern) for more "
-                            "information",
+                            "name": "Global Affected People: Refugees & Persons of Concern (2020-2024)",
+                            "description": "Refugees and Persons of Concern data (2020-2024) from HDX HAPI, please see [the documentation](https://hdx-hapi.readthedocs.io/en/latest/data_usage_guides/affected_people/#refugees-persons-of-concern) for more information",
                             "format": "csv",
-                            "resource_type": "file.upload",
-                            "url_type": "upload",
                         }
                     ]
